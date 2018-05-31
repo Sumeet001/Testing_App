ea794b24bcb103992798708538dc08dea4cd4890
sumeet <sumeetkumar001@gmail.com>
2018-05-30 21:27:53 -0400

    Add project files.

---
+++ Testing_App/Sumeet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_App
+{
+    public class Sumeet
+    {
+
+        public int Sum(int x,int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ Testing_AppTests/SumeetTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing_App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_App.Tests
+{
+    [TestClass()]
+    public class SumeetTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            Sumeet t = new Sumeet();
+            Assert.AreEqual(t.Sum(2, 3), 5);
+        }
+    }
+}
